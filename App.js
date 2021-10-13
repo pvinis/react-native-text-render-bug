0d386d6608543f9b51cb7b270622a71d4b81cafc
Pavlos Vinieratos <pvinis@gmail.com>
2021-10-13 13:49:52 +0200

    iosevka too

--- App.js
+++ App.js
@@ -1,28 +1,32 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {Text, View} from 'react-native';
 
 const App = () => {
-	const style = {
-		borderWidth: 1,
-		borderColor: 'black',
-		fontSize: 16,
-		lineHeight: 16,
-	};
-	return (
-		<View style={{flexDirection: 'row', flex: 1}}>
-			<View
-				style={{
-					flex: 1,
-					flexGrow: 1,
-					alignItems: 'center',
-					justifyContent: 'center',
-					paddingVertical: 30,
-				}}>
-				<Text style={style}>regular TEXT.</Text>
-				<Text style={style}>ALL CAPS text.</Text>
-			</View>
-		</View>
-	);
+  const style = {
+    borderWidth: 1,
+    borderColor: 'black',
+    fontSize: 16,
+    lineHeight: 16,
+  };
+  return (
+    <View style={{flexDirection: 'row', flex: 1}}>
+      <View
+        style={{
+          flex: 1,
+          flexGrow: 1,
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          marginTop: 200,
+          height: 100,
+        }}>
+        <Text style={style}>regular TEXT.</Text>
+        <Text style={style}>ALL CAPS text.</Text>
+        <Text style={[style, {fontFamily: 'Iosevka'}]}>regular TEXT.</Text>
+        <Text style={[style, {fontFamily: 'Iosevka'}]}>ALL CAPS text.</Text>
+      </View>
+    </View>
+  );
 };
 
 export default App;
